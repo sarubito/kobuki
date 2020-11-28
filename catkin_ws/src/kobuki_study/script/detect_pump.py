672ab6c53debcc3d8bea2240c9bd9e1571673f3e
sarubito <e1x18053@oit.ac.jp>
2020-11-28 17:11:46 +0900

    add detect_pump.py

---
+++ catkin_ws/src/kobuki_study/script/detect_pump.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import rospy
+from sensor_msgs.msg import Image 
+from cv_bridge import CvBridge
+import cv2
+import numpy as np
+
+def process_image(msg):
+    try:
+        bridge = CvBridge()
+        orig = bridge.imgmsg_to_cv2(msg, "bgr8")
+        img = cv2.cvtColor(orig, cv2.COLOR_BGR2HSV)
+        hsv = cv2.cvtColor(orig,cv2.COLOR_BGR2HSV)
+        lower = np.array([150,100,100])
+        higher = np.array([179,255,255])
+        img_mask = cv2.inRange(hsv,lower,higher)
+        cv2.imshow('image', img)
+        cv2.waitKey(1)
+    except Exception as err:
+        print err
+
+def start_node():
+    rospy.init_node('img_proc')
+    rospy.loginfo('img_proc node started')
+    rospy.Subscriber("/camera/rgb/image_raw", Image, process_image)
+    rospy.spin()
+
+if __name__ == '__main__':
+    try:
+        start_node()
+    except rospy.ROSInterruptException:
+        pass
