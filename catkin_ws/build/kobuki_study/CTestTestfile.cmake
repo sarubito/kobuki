712311940b35f5da375c0319d05bcd6e1324a57c
sarubito <e1x18053@oit.ac.jp>
2022-03-23 15:20:23 +0900

    WIP

--- catkin_ws/build/kobuki_study/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/user2020/ros/catkin_ws/src/kobuki_study
-# Build directory: /home/user2020/ros/catkin_ws/build/kobuki_study
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
